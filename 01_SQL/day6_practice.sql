6ec9d408c5305e0ec0fb8e37d94b06ef739f0dfb
Audrey Pao <audrey.n.wong@gmail.com>
2026-07-09 21:31:59 -0700

    add day 6 subqueries practice

---
+++ 01_SQL/day6_practice.sql
@@ -0,0 +1,30 @@
+/*Using the Orders table, write a query that returns:
+
+OrderID
+Amount
+
+where the amount is greater than the average order amount.*/
+SELECT OrderID,
+       CustomerID,
+       Amount
+FROM Orders
+WHERE Amount >
+(
+    SELECT AVG(Amount)
+    FROM Orders
+);
+
+/*Show the customers whose total spending is greater than the average total spending of all customers.*/
+
+SELECT c.Name, SUM(o.Amount) as total_spent
+FROM Customers c
+JOIN Orders o
+    ON c.CustomerID = o.CustomerID
+Group by c.CustomerID, c.Name
+Having SUM(o.Amount) >(
+    SELECT AVG(total_spending) AS average_spending
+    FROM (SELECT customerID, SUM(amount) as total_spending
+        FROM orders
+       GROUP BY customerID
+    ) AS customer_spending
+);
